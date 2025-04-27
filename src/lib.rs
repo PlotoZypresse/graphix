22e607090e6b161c3cd9f522c9600a2c861d8629
PlotoZypresse <janlucca1401@gmail.com>
2025-04-27 16:48:50 +0200

    breaking: actually works now due to adding use graphix::GraphRep; to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,2 @@
-mod graphix;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod graphix;
+pub use graphix::GraphRep;
